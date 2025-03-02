1ab334e69f9a3a8b13a1c1298bb235394124309a
Aleksandar Todorov <aleks.todorov11@gmail.com>
2025-03-02 18:43:15 +0200

    added eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    'next/core-web-vitals',
+    // any other extends you might already have
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warn' // Change from 'error' to 'warn'
+  }
+} 
