79bda38b0f22b535d3b0e54e4b419d06d27b5440
Aleksandar Todorov <aleks.todorov11@gmail.com>
2025-03-02 20:37:36 +0200

    passwordOmitted fix

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -36,7 +36,7 @@ export async function POST(req: Request) {
     })
 
     // Remove password from response
-    const { password: passwordOmitted, ...userWithoutPassword } = user
+    const { password: _, ...userWithoutPassword } = user
 
     return new NextResponse(
       JSON.stringify({
